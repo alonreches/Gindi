069c711cf6eab4a89b4522b5f8376c21714378e0
laure-s <laurescemama@gmail.com>
2017-06-14 15:30:53 +0300

    first upload

--- front/app/components/directives/directives.js
+++ front/app/components/directives/directives.js
@@ -1,4 +1,11 @@
 angular.module('Artify')
+    .directive('navbar', ['$location', '$rootScope', function ($location, $rootScope) {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/components/views/navbar.view.html',
+            controller: 'NavbarController'
+        };
+    }])
     .directive('home', ['$location', '$rootScope',
     function ($location, $rootScope) {
         return {
@@ -6,13 +13,50 @@ angular.module('Artify')
             templateUrl: 'app/components/views/home.view.html'
         };
     }])
-    .directive('navbar', ['$location', '$rootScope',
+    .directive('dialog', ['$location', '$rootScope',
         function ($location, $rootScope) {
             return {
                 restrict: 'E',
-                templateUrl: 'app/components/views/navbar.view.html'
+                templateUrl: 'app/components/views/dialog.view.html',
+                controller: 'ConversationController'
             };
         }])
+.directive('apsUploadFile', apsUploadFile)
 ;
+
+function apsUploadFile() {
+    var directive = {
+        restrict: 'E',
+        templateUrl: 'upload.file.template.html',
+        link: apsUploadFileLink
+    };
+    return directive;
+}
+
+function apsUploadFileLink(scope, element, attrs) {
+    var input = $(element[0].querySelector('#fileInput'));
+    var button = $(element[0].querySelector('#uploadButton'));
+    var textInput = $(element[0].querySelector('#textInput'));
+
+    if (input.length && button.length && textInput.length) {
+        button.click(function (e) {
+            input.click();
+        });
+        textInput.click(function (e) {
+            input.click();
+        });
+    }
+
+    input.on('change', function (e) {
+        var files = e.target.files;
+        if (files[0]) {
+            scope.fileName = files[0].name;
+        } else {
+            scope.fileName = null;
+        }
+        scope.$apply();
+    });
+}
+
 
 
