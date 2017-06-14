7625723affcb6f2ba46e226a3521d615c84ce8e4
Gilad Lumbroso <gullumbroso@gmail.com>
2017-06-14 16:32:54 +0300

    Starting to work on the results page.

--- front/app/routes.js
+++ front/app/routes.js
@@ -10,17 +10,9 @@ angular.module('Artify')
                 templateUrl: 'app/components/views/home.view.html',
                 controller: 'ConversationController'
             })
-            .when('/mail/content', {
-                templateUrl: 'app/components/views/mail-content.view.html',
-                controller: 'MailContentController'
-            })
-            .when('/mail/people', {
-                templateUrl: 'app/components/views/mail-people.view.html',
-                controller: 'MailPeopleController'
-            })
-            .when('/finish', {
-                templateUrl: 'app/components/views/finish.view.html',
-                controller: 'FinishController'
+            .when('/results', {
+                templateUrl: 'app/components/views/results.view.html',
+                controller: 'ResultsController'
             })
             .otherwise({redirectTo: '/'});
     }]);
--- front/app/services/services.js
+++ front/app/services/services.js
@@ -6,24 +6,22 @@ angular.module('Artify')
     .factory('WebService', ['$http', '$rootScope',
         function WebServiceFactory($http, $rootScope) {
 
-            var FIRST_ANSWER_URL = 'http://127.0.0.1:8000/first_answer/';
-            var MORE_QUESTIONS_URL = 'http://127.0.0.1:8000/update_data/';
-
             var service = {};
 
-            service.postFirstQuestion = postFirstQuestion;
-            service.postMoreQuestions = postMoreQuestions;
+            service.result = {
+                artwork_image: '',
+                title: 'Because',
+                artist: 'Beatles',
+                album: 'Abby Road',
+                mainGenre: 'Rock'
+            };
 
-            service.answer = [];
+            service.analyze = analyze;
 
             return service;
 
+            function analyze() {
 
-            function postFirstQuestion(firstQuestion) {
-                return $http.get(FIRST_ANSWER_URL, {params: {'firstQuestion': firstQuestion}})
             }
 
-            function postMoreQuestions(type, answerObj) {
-                return $http.get(MORE_QUESTIONS_URL, {params: {'type': type, 'answer': answerObj}})
-            }
         }]);
